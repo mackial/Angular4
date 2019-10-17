59cc32ba1e213704ddd3829afa4e36cbaef0172a
Marcial Ambriz <mackial@rebits.com.mx>
2019-10-17 16:24:19 -0500

    Conseptos basicos comprendidos

--- platzisquare/src/app/app.component.ts
+++ platzisquare/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzisquare';
+  lugares:any = [
+    {active: true, nombre: 'Floreria la Gardenia'},
+    {active: false, nombre: 'Donas la pasadita'},
+    {active: true, nombre: 'Veterinaria Huellitas Felices'}
+  ];
+  
+
+  lat:number = 19.5022012;
+  lng:number = -104.3440498;
+
+  constructor() {
+    
+  }
+
+  hacerAlgo() {
+    alert('Haciendo algo');
+  }
+
 }
--- platzisquare/src/app/app.module.ts
+++ platzisquare/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCfl7v2m8Rqwe1IJ23O743HJgA8HR8SY2o'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
