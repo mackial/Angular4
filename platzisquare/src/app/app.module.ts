3c01873ef7fd30218151acfe9383809b498c6456
Marcial Ambriz <mackial@rebits.com.mx>
2019-10-21 01:57:15 -0500

    Implementacion de Rutas en el proyecto

--- platzisquare/src/app/app.module.ts
+++ platzisquare/src/app/app.module.ts
@@ -7,12 +7,24 @@ import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
 import { ResaltarDirective } from './directives/resaltar.directive';
 import { ContarClicksDirective } from './directives/contarClicks.directive';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DetalleComponent } from './detalle/detalle.component';
+import { LugaresComponent } from './lugares/lugares.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LugaresComponent },
+  { path: 'lugares', component: LugaresComponent},
+  { path: 'detalle', component: DetalleComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ResaltarDirective,
-    ContarClicksDirective
+    ContarClicksDirective,
+    DetalleComponent,
+    LugaresComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +32,9 @@ import { ContarClicksDirective } from './directives/contarClicks.directive';
     AppRoutingModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyB8vH_BqJZOvVtADCPIWciCq5ga02SnhXI'
-    })
+    }),
+    BrowserAnimationsModule,
+      RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
